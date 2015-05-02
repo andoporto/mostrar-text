659a26cfb67eb2074ce2c8b6c9cb060492f42e27
Andres Oporto <andoporto@gmail.com>
2015-05-02 11:33:03 -0300

    subiendo

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html>
+	<head>
+		<title>Probando text</title>
+	</head>
+	<body>
+	<form  method="POST">
+		Introduzca la cadena a buscar:
+		<input type="text" name="cadena" value="valor por defecto" size="20">
+		<input type="submit" value="Aceptar">
+	</form>
+	</body>
+</html
+
+<?php
+   $cadena = $_POST['cadena']; 
+   print ($cadena);
+?>
